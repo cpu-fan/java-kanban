19bd877d6fc3d38280e2bca38433adde1ddaa111
azatvaleev <a3at.valeev@gmail.com>
2022-07-14 14:31:02 +0500

    refactor: изменить стуктуру - удалить отдельный класс CustomLinkedList и перенести его реализацию в InMemoryHistoryManager; класс Node сделать подклассом InMemoryHistoryManager

--- src/tasktracker/historymanager/InMemoryHistoryManager.java
+++ src/tasktracker/historymanager/InMemoryHistoryManager.java
@@ -3,20 +3,53 @@ package tasktracker.historymanager;
 import tasktracker.tasks.Task;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
     // Список для хранения истории просмотра задач
-    private final List<Task> historyList = new ArrayList<>();
+    private final List<Task> historyList = new ArrayList<>(); // old version, потом удалить
+
+    // (новая реализация) Хэш-таблица для хранения связки id и узлов
+    private final Map<Integer, Node> nodeMap = new HashMap<>();
+
+    private Node head;
+    private Node tail;
+
+    public void linkLast(Task task) {
+        final Node oldTail = tail;
+        final Node newNode = new Node(oldTail, task, null);
+        tail = newNode;
+        if (oldTail == null) {
+            head = newNode;
+        } else {
+            oldTail.next = newNode;
+        }
+    }
 
     @Override
     public void addToHistory(Task task) {
-        historyList.add(task);
+        linkLast(task);
+        nodeMap.put(task.getId(), tail);
+//        historyList.add(task); // old version
     }
 
     @Override
     public List<Task> getHistory() {
+//        return historyList; // old version
+        List<Task> historyList = new ArrayList<>();
+        for (Node value : nodeMap.values()) {
+            historyList.add(value.task);
+        }
+//        List<Task> historyList = new ArrayList<>();
+//        Node node = head;
+//        while (head != null) {
+//            historyList.add(node.task);
+//            if (node.next == null) break;
+//            node = node.next;
+//        }
         return historyList;
     }
 
@@ -30,5 +63,17 @@ public class InMemoryHistoryManager implements HistoryManager {
         return "InMemoryHistoryManager{" +
                 "historyList=" + historyList +
                 '}';
+    }
+
+    public static class Node {
+        private Node prev;
+        private Task task;
+        private Node next;
+
+        public Node(Node prev, Task task, Node next) {
+            this.prev = prev;
+            this.task = task;
+            this.next = next;
+        }
     }
 }
--- src/tasktracker/main/Main.java
+++ src/tasktracker/main/Main.java
@@ -1,23 +1,28 @@
 package tasktracker.main;
 
-import tasktracker.customclasses.CustomLinkedList;
 import tasktracker.managers.Managers;
 import tasktracker.taskmanager.TaskManager;
-import tasktracker.tasks.Epic;
-import tasktracker.tasks.Subtask;
 import tasktracker.tasks.Task;
 
 public class Main {
 
     public static void main(String[] args) {
-        CustomLinkedList<Task> taskCustomLinkedList = new CustomLinkedList<>();
+        TaskManager taskManager = Managers.getDefault();
 
-        taskCustomLinkedList.linkLast(new Task("name1", "desc1"));
-        taskCustomLinkedList.linkLast(new Task("name2", "desc2"));
-        taskCustomLinkedList.linkLast(new Task("name3", "desc3"));
-        taskCustomLinkedList.linkLast(new Task("name4", "desc4"));
-        taskCustomLinkedList.linkLast(new Task("name5", "desc5"));
+        Task task1 = new Task("task1", "desc for task1");
+        Task task2 = new Task("task2", "desc for task2");
+        Task task3 = new Task("task3", "desc for task3");
 
-        System.out.println(taskCustomLinkedList.size());
+        taskManager.createTask(task1);
+        taskManager.createTask(task2);
+        taskManager.createTask(task3);
+
+        taskManager.getTaskById(task1.getId());
+        taskManager.getTaskById(task2.getId());
+        System.out.println(taskManager.getHistory());
+
+        taskManager.getTaskById(task3.getId());
+        System.out.println(taskManager.getHistory());
+
     }
 }
